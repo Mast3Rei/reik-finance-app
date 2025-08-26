a9d6d74bf9d448712200e645dc91f503985f0694
Reilly Krause <89659007+Mast3Rei@users.noreply.github.com>
2025-08-25 22:26:35 -0400

    Load environment variables and update SECRET_KEY

--- reik_finance_site/reik_finance_site/settings.py
+++ reik_finance_site/reik_finance_site/settings.py
@@ -15,6 +15,7 @@ import os
 import boto3
 from boto3.dynamodb.types import DYNAMODB_CONTEXT
 from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1-7-l#!ar_00iooi5q5&t2*59kqpfpz77ond-)tl0$bdg%nj$_'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
